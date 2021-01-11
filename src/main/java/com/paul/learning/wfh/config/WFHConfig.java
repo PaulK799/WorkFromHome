df35dab437e6e891063bd956e1f161033963775e
paul.kennedy <paul.kennedy@openjawtech.com>
2021-01-11 12:06:56 +0000

    WFH-000 Initial commit

---
+++ src/main/java/com/paul/learning/wfh/config/WFHConfig.java
@@ -0,0 +1,30 @@
+package com.paul.learning.wfh.config;
+
+import com.paul.learning.wfh.WfhApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Configuration class for {@link WfhApplication}.
+ */
+@Configuration
+public class WFHConfig {
+
+    /**
+     * Bean for initalizing {@link Docket} API.
+     *
+     * @return The {@link Docket} generated.
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
